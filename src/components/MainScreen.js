fae93be3452a4fd7e2618a1b5b06ab0fc268d275
Emily Scott <62396942+escott2@users.noreply.github.com>
2021-02-16 13:26:09 -0600

    Convert sunrise from UTC with offset

--- src/components/MainScreen.js
+++ src/components/MainScreen.js
@@ -8,21 +8,25 @@ import Attribution from './Attribution';
 function MainScreen() {
 
     const today = new Date();
-    console.log(today);
 
     const [temp, setTemp] = useState("70");
     const [sunrise, setSunrise] = useState("");
     const [sunset, setSunset] = useState("");
     const [date, setDate] = useState({
         month: today.getMonth(),
-        day: today.getDate(),
+        date: today.getDate(),
         year: today.getFullYear()
     });
 
-    console.log(date);
-
     const WEATHER_API_KEY = "922176d7fe6aa80866789eaaf2e9d26d";
     const cityName = "Minneapolis";
+
+    const months = ["January","February","March","April","May","June","July",
+    "August","September","October","November","December"];
+
+    function createGMTDate(month, date, year, hour, minute, second) {
+        return new Date(`${months[month]} ${date}, ${year} ${hour}:${minute}:${second} GMT`);
+    }
 
     useEffect(() => {
         axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=imperial&appid=${WEATHER_API_KEY}`)
@@ -41,11 +45,20 @@ function MainScreen() {
 
     useEffect(() => {
 
-        axios.get(`https://api.sunrise-sunset.org/json?lat=44.9778&lng=-93.2650&date=${date.year}-${date.month}-${date.day}`)
+        axios.get(`https://api.sunrise-sunset.org/json?lat=44.9778&lng=-93.2650&date=${date.year}-${date.month + 1}-${date.date}`)
         .then(response => {
            setSunrise(() => {
-              const sunriseUTC = response.data.results.sunrise;
-              return sunriseUTC;
+
+              //Convert to military time and convert back.
+              let sunriseTime = response.data.results.sunrise.padStart(11, '0');
+              const hourUTC = Number(sunriseTime.slice(0, 2));
+              const selectedDate = new Date(date.year, date.month, date.date);
+              const offset = selectedDate.getTimezoneOffset() / 60;    
+              const hourCT = String(hourUTC + offset);
+              let sunriseTimeCT = sunriseTime.slice(2, 9);
+              sunriseTimeCT = `${hourCT}${sunriseTimeCT}`;
+              
+              return sunriseTimeCT;
           });
           setSunset(response.data.results.sunset);
         })
@@ -62,10 +75,10 @@ function MainScreen() {
     return (
         <div className="MainScreen">
             <h2>Sunrise</h2>
-            <p>{sunrise} UTC</p>
+            <p>{sunrise} AM</p>
             <h2>Sunset</h2>
             <p>{sunset} UTC</p>
-            <p>Date: {date.month}</p>
+            <p>{months[date.month]} {date.date}, {date.year}</p>
             <LightMeter temp={temp} />
             <Attribution />
         </div>
