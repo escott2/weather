9aac30c18f43a25117166e2be5f0abe5881508fb
Emily Scott <62396942+escott2@users.noreply.github.com>
2021-04-19 08:34:49 -0500

    Add responsive breakpoints and media queries

--- src/components/Temperature.js
+++ src/components/Temperature.js
@@ -14,18 +14,24 @@ function Temperature({displayTemp, currentWeather}) {
    
 
             { (isTemperature && displayTemp) &&
-                <div>
-                    <div className="flex--row">
-                        <p className="text--min-max">H: {currentWeather.highTemp}°</p>
-                        <p className="text--min-max">L: {currentWeather.lowTemp}°</p>
+                <React.Fragment>
+                    <div>
+                        <div className="flex--row">
+                            <p className="text--min-max">H: {currentWeather.highTemp}°</p>
+                            <p className="text--min-max">L: {currentWeather.lowTemp}°</p>
+                        </div>
+                    <p className="current-temp">{currentWeather.temp}°F</p>
+                    <p>Feels like: {currentWeather.feelsLike}°</p>
                     </div>
-                <p className="current-temp">{currentWeather.temp}°F</p>
-                <p>Feels like: {currentWeather.feelsLike}°</p>
-                <p>Wind: {currentWeather.windSpeed}</p>
-                <p>{currentWeather.windDirection}</p>
-                <p>Humidity: {currentWeather.humidity}%</p>
-                <p>{currentWeather.condition}</p>
-                </div>
+                    <div>
+                        <p>Wind: {currentWeather.windSpeed}</p>
+                        <p>{currentWeather.windDirection}</p>
+                    </div>
+                    <div>
+                        <p>Humidity: {currentWeather.humidity}%</p>
+                        <p>{currentWeather.condition}</p>
+                    </div>
+                </React.Fragment>
             }
 
 
