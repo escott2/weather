764464449d2339a28d08fb13aa03068180d340f4
escott2 <etriplett14@gmail.com>
2023-04-17 17:33:26 -0500

    restyle forms and inputs

--- src/components/layout/MainHeader.js
+++ src/components/layout/MainHeader.js
@@ -6,19 +6,21 @@ import "./MainHeader.scss";
 function MainHeader() {
   return (
     <header>
-      <nav className="header__nav">
-        <ul className="nav__list header__nav__list">
-          <li className="nav__list__item header__nav__list__item">
-            <NavLink to="/home/current">Home</NavLink>
-          </li>
-          <li className="nav__list__item header__nav__list__item">
-            <NavLink to="/about">About</NavLink>
-          </li>
-        </ul>
-      </nav>
-      <h1 className="title">
-        <span className="title__span">The Great </span>Outdoorcast
-      </h1>
+      <div className="header__inner-wrapper">
+        <nav className="header__nav">
+          <ul className="nav__list header__nav__list">
+            <li className="nav__list__item header__nav__list__item">
+              <NavLink to="/home/current">Home</NavLink>
+            </li>
+            <li className="nav__list__item header__nav__list__item">
+              <NavLink to="/about">About</NavLink>
+            </li>
+          </ul>
+        </nav>
+        <h1 className="title">
+          <span className="title__span">The Great </span>Outdoorcast
+        </h1>
+      </div>
     </header>
   );
 }
